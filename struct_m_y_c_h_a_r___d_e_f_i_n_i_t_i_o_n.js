e143a8c50c4da0cd77d41c742a2891ffcb21a362
TrashboxBobylev <trashbox.bobylev@gmail.com>
2025-02-09 00:29:26 +0700

    updated docs for the beta
    - added character definition docs
    - added an icon

---
+++ struct_m_y_c_h_a_r___d_e_f_i_n_i_t_i_o_n.js
@@ -0,0 +1,5 @@
+var struct_m_y_c_h_a_r___d_e_f_i_n_i_t_i_o_n =
+[
+    [ "MYCHAR_PHYSICS_DIFF", "struct_m_y_c_h_a_r___d_e_f_i_n_i_t_i_o_n_1_1_m_y_c_h_a_r___p_h_y_s_i_c_s___d_i_f_f.html", null ],
+    [ "WEAPON", "struct_m_y_c_h_a_r___d_e_f_i_n_i_t_i_o_n_1_1_w_e_a_p_o_n.html", null ]
+];
